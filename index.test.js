cdc69e67836e3f454c9addc7c0fd79e8e305f28f
Daniel W. Hieber <dwhieb@gmail.com>
2023-10-25 20:40:57 -0500

    TEST: Ignore the YAML header

--- index.test.js
+++ index.test.js
@@ -3,6 +3,11 @@ import { expect } from 'chai'
 
 import { describe, it } from 'node:test'
 
+const sample = `ninaenda
+ni-na-end-a
+1SG-PRES-go-IND
+I am going`
+
 describe(`scription2html`, function() {
 
   it(`whitespace → empty string`, function() {
@@ -10,5 +15,27 @@ describe(`scription2html`, function() {
     const output = convert(input)
     expect(output).to.equal(``)
   })
+
+  it(`ignores the YAML header`, function() {
+
+    const input = `
+    ---
+    title: The title of this story
+    ---
+    ${ sample }
+    `
+
+    const runTest = () => convert(input)
+
+    expect(runTest).not.to.throw()
+
+  })
+
+  it(`wraps utterances in <div class=igl> by default`)
+
+  it(`converts single utterances`)
+
+  it(`converts multiple utterances`)
+
 
 })
