b676f0643f7307e417a10728b51f390eb6c62f0b
Daniel W. Hieber <dwhieb@gmail.com>
2023-10-26 12:37:31 -0500

    DOCS: Add sample HTML

---
+++ build/index.js
@@ -0,0 +1,39 @@
+import convert           from '../index.js'
+import esbuild           from 'esbuild'
+import { fileURLToPath } from 'node:url'
+import path              from 'path'
+
+import { readdir, readFile, writeFile } from 'node:fs/promises'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname  = path.dirname(__filename)
+const contentTag = `<!-- CONTENT -->`
+
+// Bundle JavaScript for browsers
+
+await esbuild.build({
+  bundle:      true,
+  entryPoints: [`index.js`],
+  minify:      true,
+  outfile:     `scription2html.js`,
+})
+
+// Build samples
+
+const template = await readFile(path.resolve(__dirname, `./template.html`), `utf8`)
+const files    = await readdir(path.resolve(__dirname, `../samples/data`))
+
+for (const file of files) {
+  const scription     = await readFile(path.resolve(__dirname, `../samples/data`, file), `utf8`)
+  const filename      = path.basename(file, `.txt`)
+  let   content       = `<h1>${ filename }</h1>`
+  const { html }      = convert(scription)
+  const interlinears  = `<ul>${ html }</ul>`
+
+  content += `\n${ interlinears }`
+
+  const sample = template.replace(contentTag, content)
+
+  await writeFile(path.resolve(__dirname, `../samples/html`, `${ filename }.html`), sample)
+
+}
