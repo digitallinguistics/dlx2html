e44bf3ec4a40a9a2e4a281cf5d67b21f56952b3d
Daniel W. Hieber <dwhieb@gmail.com>
2023-10-25 21:13:23 -0500

    NEW: Returns `<div class=igl>` tag

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 export default function scription2html(input) {
 
+  if (!input) return ``
+
   const trimmed = input.trim()
 
   if (!trimmed) return ``
+
+  return `<div class=igl></div>`
 
 }
--- index.test.js
+++ index.test.js
@@ -1,7 +1,6 @@
-import convert    from './index.js'
-import { expect } from 'chai'
-
-import { describe, it } from 'node:test'
+import convert                    from './index.js'
+import { expect }                 from 'chai'
+import { parseFragment as parse } from 'parse5'
 
 const sample = `ninaenda
 ni-na-end-a
@@ -9,6 +8,11 @@ ni-na-end-a
 I am going`
 
 describe(`scription2html`, function() {
+
+  it(`no input → empty string`, function() {
+    const output = convert()
+    expect(output).to.equal(``)
+  })
 
   it(`whitespace → empty string`, function() {
     const input  = `  `
@@ -31,11 +35,18 @@ describe(`scription2html`, function() {
 
   })
 
-  it(`wraps utterances in <div class=igl> by default`)
+  it(`wraps utterances in <div class=igl> by default`, function() {
+
+    const output = convert(sample)
+    const dom    = parse(output)
+
+    expect(dom.childNodes).to.have.length(1)
+    expect(dom.childNodes[0].tagName).to.equal(`div`)
+
+  })
 
   it(`converts single utterances`)
 
   it(`converts multiple utterances`)
-
 
 })
