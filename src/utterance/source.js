ac1f520c213f07bb7a23d22df284dbf3f4950cf9
Daniel W. Hieber <dwhieb@gmail.com>
2024-03-16 16:13:50 -0500

    FIX: Do not wrap source in parentheses

--- src/utterance/source.js
+++ src/utterance/source.js
@@ -3,7 +3,7 @@ export default function createSource(speaker, source) {
   if (!(speaker ?? source)) return ``
 
   speaker ||= ``
-  source    = source ? `(${ source })` : ``
+  source  ||= ``
 
   const text = `${ speaker } ${ source }`.trim()
 
